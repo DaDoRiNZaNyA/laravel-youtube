a269ee303ad27a05265d429ab2274567871ba2b3
DaDoRiNZaNyA <aluchock@gmail.com>
2024-06-26 09:47:10 +0400

    comments crud

--- src/app/Http/Controllers/CommentController.php
+++ src/app/Http/Controllers/CommentController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use Error;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class CommentController extends Controller
 {
@@ -16,5 +18,44 @@ class CommentController extends Controller
     {
         return
             $comment->loadRelationships(request('with', []));
+    }
+
+    public function store(Request $request)
+    {
+        auth()->loginUsingId(1);
+        $attributes = $request->validate([
+            'text' => 'required|string',
+            'parent_id' => 'nullable|exists:comments,id',
+            'video_id' => 'required_without:parent_id|exists:videos,id'
+        ]);
+
+        $attributes['user_id'] = $request->user()->id;
+
+        if ($request->parent_id) {
+            $attributes['video_id'] = Comment::find($request->parent_id)->video_id;
+        }
+
+        return Comment::create($attributes);
+    }
+
+    public function update(Comment $comment, Request $request)
+    {
+        auth()->loginUsingId(1);
+        abort_if($request->user()->isNot($comment->user), Response::HTTP_UNAUTHORIZED, 'Unauthorized');
+        $attributes = $request->validate([
+            'text' => 'required|string',
+        ]);
+
+        $comment->fill($attributes)->save();
+
+        return $comment;
+    }
+
+    public function delete(Comment $comment, Request $request)
+    {
+        auth()->loginUsingId(1);
+        abort_if($request->user()->isNot($comment->user), Response::HTTP_UNAUTHORIZED, 'Unauthorized');
+
+        return Comment::destroy($comment->id);
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -26,3 +26,6 @@ Route::get('/playlists/{playlist}', [PlaylistController::class, 'show']);
 
 Route::get('/comments', [CommentController::class, 'index']);
 Route::get('/comments/{comment}', [CommentController::class, 'show']);
+Route::post('/comments', [CommentController::class, 'store']);
+Route::put('/comments/{comment}', [CommentController::class, 'update']);
+Route::delete('/comments/{comment}', [CommentController::class, 'delete']);
